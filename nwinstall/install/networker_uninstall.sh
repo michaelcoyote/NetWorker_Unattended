beac2db02a0a9cf8588f362edc5cdcc6f2371945
michael <>
2007-04-27 16:24:07 +0000

    This commit was manufactured by cvs2svn to create tag 'V0_7'.
    Sprout from master 2007-04-27 16:24:06 UTC michael 'running without restore test  and config examples'
    Delete:
        CVSROOT/checkoutlist
        CVSROOT/commitinfo
        CVSROOT/config
        CVSROOT/cvswrappers
        CVSROOT/editinfo
        CVSROOT/loginfo
        CVSROOT/modules
        CVSROOT/notify
        CVSROOT/rcsinfo
        CVSROOT/taginfo
        CVSROOT/verifymsg
        nwinstall/config/networker_install.conf
        nwinstall/config/networker_install.pl.conf
        nwinstall/install/.cvsignore
        nwinstall/install/CHANGES
        nwinstall/install/README
        nwinstall/install/cluster_config.exp
        nwinstall/install/jukebox_config.exp
        nwinstall/install/networker_install.pl
        nwinstall/install/networker_uninstall.sh

--- nwinstall/install/networker_uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ksh
-
-
-dsh -t 120 /usr/bin/networker.cluster -r
-dsh -t 120 /usr/bin/networker.cluster -r
-dsh -t 120 /usr/bin/networker.cluster -r
-
-dsh -t 120 installp -u LGTOnw.clnt.rte LGTOnw.licm.rte LGTOnw.man.rte LGTOnw.node.rte LGTOnw.serv.rte
