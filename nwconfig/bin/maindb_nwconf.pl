cee688b445debea112175383d639682fde29426f
michael <>
2007-05-01 05:38:26 +0000

    *** empty log message ***

--- nwconfig/bin/maindb_nwconf.pl
+++ nwconfig/bin/maindb_nwconf.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 ###########
-# base_nwconf.pl
+# maindb_nwconf.pl
 #
 # add the base config to networker
 #
@@ -24,8 +24,9 @@ use Getopt::Std;
 # Make our variables global
 # any new config variable belongs here
 #
-use vars qw($NSRSERVER $NSRADM $RTMP $DEBUG %options
-@CLUSTERNODES  $TMPCMDFILE $TRUN $SID $BPOLICY $RPOLICY $POOL $SAVEGROUP); 
+use vars qw($NSRSERVER $NSRADM $RTMP $DEBUG %options @CLUSTERNODES  
+$TMPCMDFILE $TRUN $SID $BPOLICY $RPOLICY $POOLNAME $SAVEGROUP $NSRMM
+$BACKPATH $BACKUP $CREATEPOOL); 
 #
 ######
 
@@ -44,7 +45,7 @@ $NSRSERVER="sdp_nsr";
 # the names of the cluster nodes primary node first
 @CLUSTERNODES=("sdp1","sdp2");
 #
-# The oracle sid YOu'll be backing up
+# The oracle DB You'll be backing up
 $SID="test1";
 #
 # set the default  brows and retention policies.
@@ -54,50 +55,51 @@ $SID="test1";
 $BPOLICY="Year";
 $RPOLICY="Year";
 #
+$POOLNAME="MainDB-backup";
 # setup the temp dir we'll use
 $RTMP="/nsr/tmp";
 #
-$TMPCMDFILE="$RTMP/nsradmin-$SID";
 #
+$BACKPATH="/nsr/savelists";
+#
+# 
+$TMPCMDFILE="$RTMP/$SID";
+
 ##### end variable default
 #
 # initialize some variables
 $DEBUG=0;
-
-$NOTIFICATION=0;
-$USERGROUP=0;
-$SCHEDULES=0;
 $TRUN=0;
+
+$BACKUP=0;
+$CREATEPOOL=0;
 
 
 #####
 # set up our options
 #
 # -D: debug
-# -n: create notifications
-# -s: create schedules
-# -u: update usergroups
-# -c: config file
 # -t: test only, works very nice with -D
+# -b: create backup config
+# -p: create pools
+# -c: config file
 #
 #
-getopts('Dtnsuc:',\%options);
+getopts('Dtbpc:',\%options);
 
 ####
 # make command line switches do something
 #
 # just makes code easier to read, really.
 if ($options{D}) { $DEBUG=1;}
-if ($options{s}) { $SCHEDULES=1;}
-if ($options{u}) { $USERGROUP=1;}
-if ($options{n}) { $NOTIFICATION=1;}
+if ($options{b}) { $BACKUP=1;}
+if ($options{p}) { $POOL=1;}
 if ($options{t}) { $TRUN=1;}
 #
 # if nothing gets selected, select all types
-if ((!$SCHEDULES) && (!$USERGROUP) && (!$NOTIFICATION)) {
-	$NOTIFICATION=1;
-	$USERGROUP=1;
-	$SCHEDULES=1;
+if ((!$options{p}) && (!$options{b})) {
+	$CREATEPOOL=1;
+	$BACKUP=1;
 }
 
 
@@ -119,9 +121,6 @@ if ($options{c}) {
 # Set up the program locations we'll use
 #
 # nsradmin location
-$NSRJB="nsradmin";
-# 
-# nsradmin
 $NSRADM="nsradmin";
 #
 # nsrmm location
@@ -137,6 +136,7 @@ print "Test Run flag set, no configuration will be performed\n" if $TRUN;
 #
 #
 #####
+#
 #
 sub create_backup_config {
 
@@ -161,22 +161,31 @@ sub create_backup_config {
 	# Create tempfile containing nsradmin commands
 	open (NSRCMD, "> $TMPCMDFILE");
 	#
-	# print it to the file$NSRTT
+	# print it to the file
 
 
-	print NSRCMD  << "CLIENT";
+	print NSRCMD  << "BACKUP";
+
+
+create type: NSR group
+name: mainDB-$SID;
+comment:Main backup for $SID;
+force incremental: No;
+success threshold: Warning;
+
+$response
 
 
 
 
 create type: NSR client;
-backup command: save -I /nsr/savelists/$SID;
-browse policy: $BPOLICY;
-comment:;
+name: $NSRSERVER;
+backup command: save -I $BACKPATH/$SID;
+comment:$SID Backup;
 group: mainDB-$SID;
-name: sdp_nsr;
 remote access: $cl_admprivs_print;
 retention policy: $RPOLICY;
+browse policy: $BPOLICY;
 save set: /nsr/savelists/$SID;
 schedule: FullAlways;
 
@@ -184,13 +193,80 @@ $response
 
 
 
-create type: NSR schedule;
-action: full full full full full full full;
-comment:Comverse full always schedule;
-name: FullAlways;
-period: Week;
+
+BACKUP
+
+	print NSRCMD "quit\n";
+	close (NSRCMD);
+	
+	# call nsrdadmin and use the tempfile
+	#
+	print "running nsradmin\n";
+	
+	open (NSRADMIN, "$NSRADM  -i $TMPCMDFILE-b 2>&1|") || die "Cannot start nsradmin: $!\n";
+	my @nsradm=<NSRADMIN>;
+	close (NSRADMIN);
+	print "nsradmin out:\n @nsradm \n\n" if $DEBUG;
+
+	#
+	#
+	foreach my $nsradm_ln (@nsradm) {
+		if ($nsradm_ln =~ m/.*failed.*already.exists.*/) {
+			print "c";
+			print "onfigured: $nsradm_ln\n" if $DEBUG;
+			my @config_ln = split(":", $nsradm_ln);
+			push (@preconfigured, $config_ln[1]);
+		} if ($nsradm_ln =~ m/.*failed.*/) {
+			print "$nsradm_ln\n";
+			die "nsradm failed\n";
+		}
+	} 
+
+
+	# remove the temp command file
+	#
+	if ((!$DEBUG) && (!$TRUN)) {
+		unlink ($TMPCMDFILE-b);
+	}
+
+}
+
+
+
+#####
+#
+#
+sub create_pool_config {
+
+	my $response="y";
+
+	if ($TRUN) { $response="n"; }
+
+	# Create tempfile containing nsradmin commands
+	open (NSRCMD, "> $TMPCMDFILE-p");
+	#
+	# print it to the file
+
+
+	print NSRCMD  << "CLIENT";
+
+
+
+
+create type: type: NSR pool;
+name: $POOLNAME;
+groups:mainDB-$SID;
+label template: Default;
+pool type: Backup;
+Recycle from other pools: No;
+Recycle to other pools: No;
+store index entries: Yes;
 
 $response
+
+
+
+
 
 CLIENT
 
@@ -218,17 +294,25 @@ CLIENT
 			print "$nsradm_ln\n";
 			die "nsradm failed\n";
 		}
+
+
 	} 
 
 
 	# remove the temp command file
 	#
-	#unlink ($TMPCMDFILE);
+	if ((!$DEBUG) && (!$TRUN)) {
+		unlink ($TMPCMDFILE-p);
+	}
 
 }
+#
+#
+#####
 
 
 
+create_backup_config() if $BACKUP;
 
-create_backup_config();
 
+create_pool_config() if $CREAATEPOOL;
