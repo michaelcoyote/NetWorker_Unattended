f5a6029d46e2714d9b4e8b35172e3967e14ee2ac
michael <>
2007-05-03 18:05:16 +0000

    This commit was manufactured by cvs2svn to create tag 'V1_0'.
    Sprout from master 2007-05-01 05:38:26 UTC michael '*** empty log message ***'
    Cherrypick from master 2007-05-03 18:05:15 UTC michael 'fixed file name for saveset':
        nwprotect/bin/main_backup.pl
        nwprotect/config/mainDB-backup.cfg
    Delete:
        CVSROOT/checkoutlist
        CVSROOT/commitinfo
        CVSROOT/config
        CVSROOT/cvswrappers
        CVSROOT/editinfo
        CVSROOT/loginfo
        CVSROOT/modules
        CVSROOT/notify
        CVSROOT/rcsinfo
        CVSROOT/taginfo
        CVSROOT/verifymsg
        nwinstall/config/networker_install.conf
        nwinstall/config/networker_install.pl.conf
        nwinstall/install/.cvsignore
        nwinstall/install/CHANGES
        nwinstall/install/README
        nwinstall/install/cluster_config.exp
        nwinstall/install/jukebox_config.exp
        nwinstall/install/networker_install.pl
        nwinstall/install/networker_uninstall.sh

--- nwprotect/bin/main_backup.pl
+++ nwprotect/bin/main_backup.pl
@@ -31,16 +31,16 @@ use Getopt::Std;
 # Make our variables global
 # any new config variable belongs here
 #
-use vars qw($NSRSERVER $NSRJB $MMINFO $SAVEGRPCMD $SVGRP_ROOT $SVGRP 
-	$NWSCH $FSSSN $BUINLST $MAXBACKUPS $BKUPFRQ $SSFN $DEBUG
+use vars qw($NSRSERVER $NSRJB $MMINFO $SAVEGRPCMD $GROUPNAME $LOGFILE $LOGLOC
+	$SCHEDULE $FSSSN $BUINLST $MAXBACKUPS $BKUPFRQ $SSFILENAME $DEBUG
 	$NSRMM $opt_D %options $SYSTIME $NOCHECK $NOLIST $NOBKUP 
-	$RECOVER $RTMP $BUCKSUM $CHECKSUM); 
+	$RECOVER $RTMP $BUCKSUM $CHECKSUM $BACKPATH $NORECOVERTEST
+	$LOGRETAIN); 
 #
 ######
 
 #####
-# set the defaults here and copy block to
-# "/usr/ngscore/config/'progname'.conf" file
+# set the defaults here and copy block to your conf file
 # 
 ## ## start config block ##
 # 
@@ -48,22 +48,27 @@ use vars qw($NSRSERVER $NSRJB $MMINFO $SAVEGRPCMD $SVGRP_ROOT $SVGRP
 #
 # the Name of the clustered
 # NetWorker server
-$NSRSERVER="stinkpad-ii";
+$NSRSERVER="sdp_nsr";
 #
 # Saveset file name
-$SSFN="bu_list";
+# this should be set to an oracle sid or 
+# some other useful designation 
+$SSFILENAME="bu_list";
+#
 # fileset location/saveset name
-$FSSSN="/nsr/savelists/$SSFN";
+$FSSSN="/nsr/savelists/$SSFILENAME";
+# 
+# SaveGroup name
+$GROUPNAME="main_db-test1";
 #
-$SVGRP="main_db-test1";
+# Backups Path
+$BACKPATH="/nsr/savelists";
+# 
 # backup in list
-$BUINLST="/nsr/savelists/$SSFN-in";
-#
-# Backup checksum file
-$BUCKSUM="/nsr/savelists/$SSFN-checksum";
-#
+$BUINLST="/backup_vol/test1_backuplist";
+# 
 # NW Backup Schedule
-$NWSCH="FullAlways";
+$SCHEDULE="FullAlways";
 #
 # number of backups to keep
 $MAXBACKUPS=3;
@@ -72,6 +77,13 @@ $MAXBACKUPS=3;
 # in seconds (e.g. 3660 = 1hr, 43920 = 12 hr, 87840 = 1 day)
 $BKUPFRQ=3660;
 #
+# What is our log location
+$LOGLOC="/var/tmp";
+#
+# Remove logs over n days old
+# set to 0 to skip removal
+$LOGRETAIN="10";
+#
 #
 $RTMP="/nsr/tmp/";
 #
@@ -79,6 +91,7 @@ $RTMP="/nsr/tmp/";
 $NOCHECK=0;
 $NOLIST=0;
 $NOBKUP=0;
+$NORECOVERTEST=0;
 #
 ##### end variable default
 #
@@ -90,6 +103,8 @@ $CHECKSUM=0;
 # set it up once and operate on it 
 # to avoid race condtions mostly
 $SYSTIME=time();
+
+
 
 #####
 # set up our vars
@@ -108,11 +123,11 @@ if ($options{c}) {
 	if ( -e "$options{c}") { 
 		print "reading $options{c}\n";
 		no strict 'refs';
-		open ( CONF, "$options{c}") || die "cannot open config: $!\n";
+		open ( CONF, "$options{c}") || die ("cannot open config: $!\n");
 		my $conf = join "", <CONF>; 
 		close CONF;
 		eval $conf;
-		die "Couldn't eval config file: $@\n" if $@;
+		die"Couldn't eval config file: $@\n" if $@;
 		print "config file loaded\n";
 	} else {print "Config file not found, using defaults\n";}
 }
@@ -146,8 +161,104 @@ if ($options{'f'}) {
 	$BKUPFRQ=0;
 }
 #####
+#
+#
+# Log function courtesy of Laramee Gerard <Gerard.Laramee@comverse.com>
+#
+$LOGFILE="$LOGLOC/mainbackup$$.log";
+
+print "using $LOGFILE as the logfile\n";
+# Log function you might find at your local Wal-Mart
+sub Log {
+	my %args = (
+		'sub'            => '',
+		'message'        => '',
+		'level'          => 'INFO',
+                @_,
+	);
+	my $date=`/usr/bin/date`;
+	my $use_stdout=0;
+	$use_stdout=1 unless(open(FILE, ">>$LOGFILE"));
+
+	chomp($date);
+	if ( $use_stdout ) {
+		printf("%s %s: %s\n",$date,$args{level},$args{message}) if ($args{sub} eq '');
+		printf("%s [%s] --> %s: %s\n",$date,$args{sub},$args{level},$args{message}) if ($args{sub} ne '');
+	}
+	else {
+		printf(FILE "%s %s: %s\n",$date,$args{level},$args{message}) if ($args{sub} eq '');
+		printf(FILE "%s [%s] --> %s: %s\n",$date,$args{sub},$args{level},$args{message}) if ($args{sub} ne '');
+		close(FILE);
+
+	}
+}
+sub Die {
+ 
+	my ($status,$error) = @_;
+	
+	$ENV{ERROR} = $error if defined $error;
+	$ENV{STATUS} = $status if defined $status;
+ 
+	Exit ();
+ 
+}
+
+sub Exit {
+	my ($status,$info) = @_;
+ 
+	$ENV{STATUS} = $status if defined $status;
+	$ENV{INFO} = $info if defined $info;
+	$ENV{STATUS} = -1 unless exists $ENV{STATUS};
+ 
+	print "STATUS=$ENV{STATUS}\n" if exists $ENV{STATUS};
+	print "INFO=$ENV{INFO}\n" if exists $ENV{INFO} and $ENV{INFO};
+	print "ERROR=$ENV{ERROR}\n" if exists $ENV{ERROR} and $ENV{ERROR};
+	exit $ENV{STATUS};
+}
+###
+#
+# a quick function to log and die a process on error
+sub DieNoisy {
+	my ($sub,$status,$error) = @_;
+	
+	Log(sub => "$sub",
+		message => "$error", 
+		level => 'ERROR');
+	
+	$ENV{ERROR} = $error if defined $error;
+	$ENV{STATUS} = $status if defined $status;
+ 
+	Exit ();
+ 
+}
+#
+###
+
+###
+#
+sub logremove {
+	my @findout = `find $LOGLOC -type f -name mainbackup*.log -mtime +$LOGRETAIN`;
+
+	if (@findout) {
+
+		foreach my $file (@findout) {
+        		print ("removing $file\n");
+			Log(sub => "logremove", message => "removing $file");
+        		unlink ($file);
+		}
+	} else {
+		Log(sub => "logremove", message => "no logfiles to remove");
+	}
+
+}
+#
+###
+
+####
+Log(message => 'Backup Starting');
 
 print "Debug set\n" if $DEBUG;
+Log(message => 'Debug set', level => 'DEBUG') if $DEBUG;
 
 # 
 
@@ -158,33 +269,20 @@ sub recycler {
 	# -v verbose
 	# -y "yes", approves any action
 	# -S specifies the ssid or ssid\cloneid pair
-	open (RCYC, "$NSRMM -s $NSRSERVER -dvy -S $_[0] 2>&1|") || die "recycling $_[0] failed\n";
+	open (RCYC, "$NSRMM -s $NSRSERVER -dvy -S $_[0] 2>&1|") || DieNoisy ('recycler',1,"recycling $_[0] failed");
 	my @re_in = <RCYC>;
 	foreach my $re_ln (@re_in) {
 		if ($re_ln =~ m/.*not.in.the.media.index.*/) {
-			print "SSID $_[0] not in media index. already deleted?";
+			Log(sub => 'recycler', message => "SSID $_[0] not in media index. already deleted?", level => 'WARNING');
+			print "SSID $_[0] not in media index. already deleted?\n";
 		} if ($re_ln =~ m/.*unknown.host.*/) {
-			die "$NSRSERVER not found or unroutable\n";
+			Log(sub => 'recycler', message => "$NSRSERVER not found or unroutable",level => 'ERROR');
+			Die(1,"$NSRSERVER not found or unroutable");
 		} else {
 			print "SaveSet $_[0] removed\n";
 			next;}
-
 	}
 }
-
-
-sub checksum {
-        #create a SysV style checksum
-	# same as "sum -o file" on AIX
-        my $sum=0;
-        open (CKFILE, "$_[0]") || die "cant open $_[0]\n";
-        while (<CKFILE>){
-                $sum += unpack("%16C*",$_);}
-        $sum %= (2 ** 16) - 1;
-        close (CKFILE);
-        return ($sum);
-}
-	
 
 #####
 #
@@ -199,7 +297,7 @@ sub media_ck {
 	#my $query="-q \"!incomplete\"";
 	my $query="-q name=\"$FSSSN\" -q \"!incomplete\"";
 	my $report="-r ssid,cloneid,name,nsavetime,savetime,volume,sumsize,client";
-	open (SSIN, "$MMINFO -xc, $query $report 2>&1|") || die "$MMINFO failed: $!"; 
+	open (SSIN, "$MMINFO -xc, $query $report 2>&1|") || DieNoisy('media_ck',1,"$MMINFO failed: $!"); 
 	my @ss_in = <SSIN>;
 	my @ss_mid;
 	my %ss_out;
@@ -207,10 +305,12 @@ sub media_ck {
 	my $bu_ave; # for estimating usage
 	my @bu_mfd; # record SSIDs marked for deletion
 	print "mminfo output:\n@ss_in\n" if $DEBUG;
+	Log(sub => 'media_ck', message => "mminfo output:\n@ss_in",level => 'DEBUG') if $DEBUG;
 	foreach my $ss_ln (@ss_in) {
 		chomp($ss_ln);
-		if ($ss_ln =~ m/.*$SSFN.*/) { #temp sorting of mminfo
+		if ($ss_ln =~ m/.*$SSFILENAME.*/) { #temp sorting of mminfo
 			print "$ss_ln\n" if $DEBUG;
+			Log(sub => 'media_ck', message => "$ss_ln",level => 'DEBUG') if $DEBUG;
 			my ($ssid, $clnid, $name, $nsavetime, 
 				$savetime, $volume, $sumsize,$client
 			) =  split(',', $ss_ln);
@@ -218,11 +318,16 @@ sub media_ck {
 			$sumsize =~ s/\ MB/000000/;
 			$sumsize =~ s/\ GB/000000000/;
 			print "backup size: $sumsize\n" if $DEBUG;
+			Log(sub => 'media_ck', message => "backup size: $sumsize",level => 'DEBUG') if $DEBUG;
 
-			if (($SYSTIME - $BKUPFRQ) < $nsavetime) {
+			if (($SYSTIME - $BKUPFRQ) < $nsavetime) {  
+				Log(level => 'WARNING',sub => 'media_ck', message => "systime: $SYSTIME\nBackup Limit:$BKUPFRQ\nsavetime: $nsavetime, backup within time limit, wait to run or force with -f");
 				print "systime: $SYSTIME\nBackup Limit:$BKUPFRQ\nsavetime: $nsavetime\n";
 				print "backup within time limit, wait to run or force with -f\n";
-				exit;
+				# TODO: set exit status to warning
+				# i.e $ENV{STATUS} = $WARNING; 
+				$ENV{INFO} = "systime: $SYSTIME  Backup Limit:$BKUPFRQ  savetime: $nsavetime, backup within time limit, wait to run or force with -f";
+				Exit;
 			}
 			push(@{$ss_out{$nsavetime}},
 				$ssid."/".$clnid,
@@ -241,14 +346,15 @@ sub media_ck {
 		print "r";
 		++ $bu_count;
 		my @ss_ref = @{$ss_out{$k}};
-		#print "\n\nStuff: @{$ss_out{$k}}\n\n";
 		my ($ssid, $name, $nsavetime, $savetime, 
 			$volume, $sumsize, $client
 		) =  @{$ss_out{$k}};
 		print "- backup $name from $k being processed \n" if $DEBUG;
+		Log(sub => 'media_ck', message => "backup $name from $k being processed",level => 'DEBUG') if $DEBUG;
 		if ($MAXBACKUPS <= $bu_count) {
 			print "o";
 			print "- SSID $ssid queued for removal\n" if $DEBUG;
+			Log(sub => 'media_ck', message => "SSID $ssid queued for removal",level => 'DEBUG') if $DEBUG;
 			push (@bu_mfd, $ssid);
 		}
 		print "$bu_count";
@@ -256,16 +362,13 @@ sub media_ck {
 	print "\n\n";	
 	foreach my $ssd (@bu_mfd) {
 		print "recycling SaveSet: $ssd\n";
+		Log(sub => 'media_ck', message => "recycling SaveSet: $ssd");
 		recycler($ssd); ## take out the cans and bottles
-	
 
 	}
 	
-
-
 }
 # if available media – permit backup 
-# if no available media recycle media 
 # *and* there is at least 1 (2?) valid backup if not send SNMP trap 
 # or error
 #
@@ -280,9 +383,9 @@ sub media_ck {
 # read in full file list
 sub backup_list {
 	print "creating filelist for $FSSSN\n";
-	open (BU_INFILE, "$BUINLST") || die "Error reading $BUINLST, stopped: $!\n";
+	Log(sub => 'backup_list', message => "creating filelist for $FSSSN");
+	open (BU_INFILE, "$BUINLST") || DieNoisy('backup_list',1, "Error reading $BUINLST, stopped: $!");
 	my @backupset_in = <BU_INFILE>;
-	
 	my @backupset_out; # put the files here
 	my @backupck_out; # put the files here
 	my @fnf; # missing files for backup report
@@ -302,24 +405,26 @@ sub backup_list {
 	# no point in backing up an empty file.
 	# error out and let people know
 	if (!@backupset_out) {
-		die "backup set empty";
+		DieNoisy('backup_list',1,"backup file set empty");
 	}
-	# create test file w/ timestamp & filelist
-	# for now add the backup file to the list
+	# for now add the backup files to the list
 	push (@backupset_out,$BUINLST);
-	push (@backupset_out,$bkupfileout);
+	push (@backupset_out,$FSSSN);
 	#
 	# write the actual backup list
-	open (BU_OUTFILE, "> $FSSSN") || die "Error writing $FSSSN, stopped: $!\n";
+	open (BU_OUTFILE, "> $FSSSN") || DieNoisy ('backup_list',1,"Error writing $FSSSN, stopped: $!\n");
 	
 	foreach my $bkupfileout (@backupset_out){
+		print "$bkupfileout\n" if $DEBUG;
 		print BU_OUTFILE "$bkupfileout\n";
 	}
 	# 
 	# TODO
 	# this array could go somewhere either log or email or both
 	if (@fnf) { print "these files not found:\n ";
+		Log(sub => 'backup_list', message => "these files not found:",level => 'WARNING');
 		foreach my $f (@fnf){
+		Log(sub => 'backup_list', message => "$f",level => 'WARNING');
 		print "$f\n"}
 	}
 	print "file: $FSSSN created\n";
@@ -329,7 +434,7 @@ sub backup_list {
 sub savegrp {
 	print "starting backup to tape\n";
 
-	open (SAVEGRP, "$SAVEGRPCMD -v -C $NWSCH -G $SVGRP 2>&1|" ) || die "$SAVEGRPCMD failed: $!";
+	open (SAVEGRP, "$SAVEGRPCMD -v -C $SCHEDULE -G $GROUPNAME 2>&1|" ) || DieNoisy('savegrp',1, "$SAVEGRPCMD failed: $!");
 
 	my @svgrp_in = <SAVEGRP>;
 	my @svgrp_succeed;
@@ -351,7 +456,7 @@ sub savegrp {
 			push (@svgrp_fail, $svg_ln)
 		} if ($svg_ln =~ m/.*no.group.named.*/) {
 			push (@svgrp_fail, $svg_ln)
-		} if ($svg_ln =~ m/.*$SSFN.*/) {
+		} if ($svg_ln =~ m/.*$SSFILENAME.*/) {
 			$svg_ln =~ s/.*\://;
 			push (@svgrp_slist, $svg_ln);
 		} else { next; #print "uncaught line: $svg_ln\n"
@@ -360,52 +465,124 @@ sub savegrp {
 
 	print "success report:\n\n @svgrp_succeed\n";
 
-	print "failure report:\n @svgrp_fail\n" if @svgrp_fail ;
+	if (@svgrp_fail) {
+		print "failure report:\n @svgrp_fail\n" if @svgrp_fail ; 
+		DieNoisy('savegrp',1,"failure report:\n @svgrp_fail");
+	}	
 
 	print "NetWorker Backup File List:\n\n @svgrp_slist\n" if @svgrp_slist ;
 }
+#
+#
+#####
+
+#####
+# recover()
+# takes in full pathname and
+# returns the path to the recovered file
+#
 
 sub recover {
-	open (RTEST, "$RECOVER -s $NSRSERVER -d $RTMP -a $_[0]  2>&1|") || die "Recovery failed: $!\n";
-	my @recovtest = <RTEST>;
-	my $r_file;
-	foreach my $r_ln (@recovtest) {
-		if ($r_ln =~ m/.*$SSFN$/) {
-			chomp($r_ln);
-			$r_ln = $r_file;
-		} if ($r_ln =~ m/^Received.1.file.*/) {
-			print "recovery suscessful\n";
-			return($r_file);
-		} else {next;}
-		print "problem with recovery\n";
-	}
-}
+	print "recovering $_[0]\n";
+	Log(sub => 'recover', message => "recovering $_[0]");
+        open (RTEST, "$RECOVER -s $NSRSERVER -f -d $RTMP -a $_[0]  2>&1|") || DieNoisy('recover',1,"Recovery failed: $!");
+        my @recovtest = <RTEST>;
+	print "DEBUG recover output\n @recovtest \n" if $DEBUG;
+	Log(sub => 'recover', message => "DEBUG recover output\n @recovtest",level => 'DEBUG');
+        my $r_file;
+        foreach my $r_ln (@recovtest) {
+                chomp($r_ln);
+                if ($r_ln =~ m/^Received.1.file.*/) {
+                        print "recovery successful\n"; 
+			Log(sub => 'recover',message => 'recovery successful');
+                        return($r_file);
+                } if ($r_ln =~ m/^Nothing.to.recover.*/) {
+                        print "recovery failed\n";
+			DieNoisy('recover',1,"problem with recovery, file not found. recover output:\n @recovtest");
+                        return($r_file);
+                } else {next;}
+                print "problem with recovery\n";
+		print "recover output:\n @recovtest\n";
+		DieNoisy('recover',1,"problem with recovery, recover output:\n @recovtest");
 
+        }
+}
+#
+#
+#####
+
+#####
+# checksum()
+# takes in a file and returns a
+# old bsd style checksum
+#
+sub checksum {
+        #create a SysV style checksum
+        # same as "sum -o file" on AIX
+	print "entering checksum subroutine\n";
+	Log(sub => 'checksum', message => "entering checksum subroutine");
+        my $sum=0;
+        open (CKFILE, "$_[0]") || DieNoisy('checksum',1,"cant open file for checksumming $_[0]\n");
+	print "creating checksum for $_[0]\n";
+        while (<CKFILE>){
+                $sum += unpack("%16C*",$_);}
+        $sum %= (2 ** 16) - 1;
+        close (CKFILE);
+        return ($sum);
+}
+#
+#
+#####
+
+#####
+# filetest()
+# restores a file compares with its
+# original via a checksum
+#
 sub filetest {
-	my $testfile recover($BUCKSUM);
+        use File::Basename;
+        recover($_[0]);
+        my $rfile = basename($_[0]);
+	my $ofilesum = checksum($_[0]);
+	print "checksum for $_[0]: $ofilesum\n";
+	Log(sub => 'filetest', message => "checksum for $_[0]: $ofilesum");
+	my $rfilesum = checksum("$RTMP/$rfile");
+	print "checksum for $RTMP/$rfile: $rfilesum\n";
+	Log(sub => 'filetest', message => "checksum for $RTMP/$rfile: $rfilesum");
 
-}
+        if (($ofilesum)&&($rfilesum)&&($ofilesum eq $rfilesum )) {
+                print "restore checksum passed\n";
+		Log(sub => 'filetest', message => "restore checksum passed");
+                return(0);
+        } else {
+                print "restore checksum failed\n";
+		Log(sub => 'filetest', message => "restore checksum failed", level => 'ERROR');
+                return(1);
+        }
+} ## end filetest sub
+#
+#
+#####
 
 #######
 # main section of program
 #
 
-
+# if $LOGRETAIN is nonzero rin the subroutine
+logremove() if ($LOGRETAIN);
+#
+# media check for savesets
 media_ck() if (!$NOCHECK);
+#
+# create the backuplist that will be used as the saveset
 backup_list() if (!$NOLIST);
+# 
+# start the actual save
 savegrp() if (!$NOBKUP);
-#filetest($BUCKSUM)
-
 # 
-#
-# if failure – recycle SSID and send SNMP trap 
-# 
-#
-#
-#
-# Media validation test – restore files to test and log 
-# if fail – recycle media and send SNMP trap 
+# do a recovery test on the recovered $BUINLIST
+filetest($BUINLST) if (!$NORECOVERTEST);
 
-
-
-
+#
+# end
+#### 
